f198a64c3858aff600d016f8ce3e4b99e1b19169
Ryuk <ryuk.infosec@gmail.com>
2022-12-18 21:49:06 +0530

    Now program support Loop
    Implimented loop

--- cmd/test-release/main.go
+++ cmd/test-release/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 	c "github.com/0xRyuk/test-release/internal/core"
+	u "github.com/0xRyuk/test-release/internal/util"
 )
 
 func main() {
 	fmt.Println("Test-release")
 	fmt.Println("Square of 5 is", c.Square(5))
 	name("Ryuk")
+	u.Loop()
+
 }
 
 func name(str string) {
